e3a2eaf5a19f8dc993fc0cfba3a9a04757b43675
Nicolau NP <mr1Normaliii@gmail.com>
2023-03-05 16:15:29 +0000

    update

--- src/app/components/estudantes/estudante-list/estudante-list.component.ts
+++ src/app/components/estudantes/estudante-list/estudante-list.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Title } from '@angular/platform-browser';
+import { ApiRequestService } from './../../../services/api-request.service';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-estudante-list',
   templateUrl: './estudante-list.component.html',
   styleUrls: ['./estudante-list.component.css']
 })
-export class EstudanteListComponent {
+export class EstudanteListComponent implements OnInit {
+
+  data = {
+    menu: 'Estudantes',
+    submenu: 'Listar',
+    type: 'estudantes'
+  }
+
+  constructor(@Inject(DOCUMENT) private document: Document, private renderer: Renderer2, private title: Title, private api: ApiRequestService) {
+    title.setTitle('[INSCRITOR] - Listar Estudantes')
+  }
+
+  ngOnInit(): void {
+    this.renderer.addClass(this.document.body, 'main')
+  }
+
+  ngOnDestroy() {
+    this.renderer.removeClass(this.document.body, 'main')
+  }
 
 }
